daf0036591e131797082a739f678e9c9b3e88776
angelo-lepore <angelo.lepore94@gmail.com>
2025-08-03 12:30:13 +0200

    fix: update show function to handle movie retrieval and include reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -27,18 +27,29 @@ function show(req, res) {
   // definiamo la query SQL per selezionare un elemento dalla tabella "movies" con l'ID specificato
   const sql = "SELECT * FROM movies WHERE id = ? ;";
   // esegue la query sul database, passando l'ID come parametro
-  connection.query(sql, [id], (err, results) => {
+  connection.query(sql, [id], (err, result) => {
     // se si verifica un errore durante la connessione o l'esecuzione della query
     if (err) return res.status(500).json({ error: "Database query failed" });
     // se non viene trovato alcun risultato (l'ID non esiste nella tabella "movies"),
-    if (results.length === 0)
-      return res.status(404).json({ error: "Post not found" });
-    // aggiungiamo il percorso completo dell'immagine per ogni film
-    results.map((item) => {
-      item.image = `${process.env.SERVER_URL}/imgs/movies_cover/` + item.image;
+    if (result.length === 0)
+      return res.status(404).json({ error: "Movie not found" });
+    // salviamo il film ottenuto dalla query
+    const movie = result[0];
+    // definiamo la query SQL per ottenere tutte le recensioni associate al film
+    const reviewsSql = "Select * from reviews WHERE movie_id=?";
+    // eseguiamo la query per ottenere le recensioni del film
+    connection.execute(reviewsSql, [id], (err, result) => {
+      // se si verifica un errore durante l'esecuzione della query delle recensioni, restituiamo un errore 500
+      if (err) return res.status(500).json({ error: "Database query failed" });
+      // salviamo le recensioni ottenute
+      const movieReviews = result;
+      // aggiungiamo le recensioni all'oggetto "movie"
+      movie.reviews = movieReviews;
+      // aggiungiamo il percorso completo dell'immagine del film
+      movie.image = `${process.env.SERVER_URL}/imgs/movies_cover/${movie.image}`;
+      // restituiamo il film (con le recensioni e l'immagine aggiornata) come risposta JSON
+      return res.json(movie);
     });
-    // se l'elemento è stato trovato, lo restituisce come risposta JSON
-    return res.json(results[0]);
   });
 }
 
